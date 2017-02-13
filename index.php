4df97b36cb85d6dcef66deab411ec475db0ded92
Brett Gilbert <bgilb13@gmail.com>
2017-02-13 16:45:16 -0700

    did bootstrap w/o css other than sticky footer

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<!-- MY Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<title>Bootstrap Challenge</title>
+	</head>
+
+
+	<body class="sfooter">
+
+		<div class="sfooter-content">
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<div class="container-fluid">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+										  data-target="#main-menu" aria-expanded="false">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="https://www.reddit.com/r/all/top/" target="_blank">Logo</a>
+							</div>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="main-menu">
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="https://bootcamp-coders.cnm.edu/class-materials/bootstrap/" target="_blank">Button
+											1</a></li>
+									<li><a href="http://getbootstrap.com/css/#images" target="_blank">Button 2</a></li>
+									<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/bootstrap-practice/?choice=yes"
+											 target="_blank">Button 3</a></li>
+									<li><a href="https://www.pandora.com/" target="_blank">Button 4</a></li>
+								</ul>
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</div>
+				</nav>
+			</header>
+			<main>
+				<div class="container">
+					<div class="container-fluid">
+						<div class="text-center">
+							<div class="row">
+								<h1>Welcome Message</h1>
+								<p>I just wanna die! Lick, lickity, lick my balls! Oh my god, how could I not see this coming?
+									My lust, my greed! I deserve thiiiiii- Ladies, let's get out of here. I haven't learned a
+									thiiiiiing! What the fuck is going on?!</p>
+							</div>
+						</div>
+					</div>
+					<!-- row 1 -->
+					<div class="row">
+						<div class="col-md-3">
+							<img src="http://www.fillmurray.com/g/150/150">
+						</div>
+						<div class="col-md-9">
+							<div class="text-center">
+								<p>Oh I say good sir, oh harumph, ohh a bope-a-dope-o-bobo. I am not putting my father in a
+									home! He just came back into my life, and you want to, grab him and, stuff him under a
+									mattress like last month's Victoria's Secret?! Puffy vagina. its not like we can do this
+									every week, we get 3 or 4 more of these tops.</p>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="text-center">
+							<div class="col-md-3">
+								<img src="http://www.fillmurray.com/g/100/100" class="img-circle">
+								<h2>Heading</h2>
+								<p>I'll be with Reuben in my workshop while you guys are having another day in Phil Collin's
+									proverbial paradise. You don't know me! They're not infinite universes left in sync with the
+									show. Nothing you do matters, your existance is a lie!</p>
+							</div>
+							<div class="col-md-3">
+								<img src="http://www.fillmurray.com/g/100/100" class="img-circle">
+								<h2>Heading</h2>
+								<p>I'll be with Reuben in my workshop while you guys are having another day in Phil Collin's
+									proverbial paradise. You don't know me! They're not infinite universes left in sync with the
+									show. Nothing you do matters, your existance is a lie!</p>
+							</div>
+							<div class="col-md-3">
+								<img src="http://www.fillmurray.com/g/100/100" class="img-circle">
+								<h2>Heading</h2>
+								<p>I'll be with Reuben in my workshop while you guys are having another day in Phil Collin's
+									proverbial paradise. You don't know me! They're not infinite universes left in sync with the
+									show. Nothing you do matters, your existance is a lie!</p>
+							</div>
+							<div class="col-md-3">
+								<img src="http://www.fillmurray.com/g/100/100" class="img-circle">
+								<h2>Heading</h2>
+								<p>I'll be with Reuben in my workshop while you guys are having another day in Phil Collin's
+									proverbial paradise. You don't know me! They're not infinite universes left in sync with the
+									show. Nothing you do matters, your existance is a lie!</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="text-center">Copyright Text</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
